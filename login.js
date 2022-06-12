b9fc0e4c1dcab26497399561091f27c3a9837843
dangk4atwit <60703094+dangk4atwit@users.noreply.github.com>
2022-06-12 13:13:51 -0400

    alert

--- login.js
+++ login.js
@@ -1,4 +1,6 @@
 
+window.alert = null;
+
 
 // Executes on click of login button.
 function validate() {
@@ -11,10 +13,12 @@ function validate() {
     if ( username == "user" && password == "user"){
         alert ("Login successfully");
         window.location = "success.html"; // Redirecting to other page.
+        delete window.alert;
     }
     else if ( username == "admin" && password == "admin"){
         alert ("Login successfully");
         window.location = "sucess.html"; // Redirecting to other page.
+        delete window.alert;
     }
     else {
         alert ("Log in failed. Please Try Again: ");
