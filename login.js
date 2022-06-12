64b9051871e0a89ab8d49fb74129aea9b8d95acb
dangk4atwit <60703094+dangk4atwit@users.noreply.github.com>
2022-06-12 13:01:09 -0400

    added user and admin pofiles

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 
+
 // Executes on click of login button.
 function validate() {
     var username = document.getElementById("Uname").value;
@@ -7,10 +8,13 @@ function validate() {
     // clear values after submit button is pressed
     document.getElementById("password").value = "";
 
-    if ( username == "admin" && password == "admin"){
+    if ( username == "user" && password == "user"){
         alert ("Login successfully");
         window.location = "success.html"; // Redirecting to other page.
-        return false;
+    }
+    else if ( username == "admin" && password == "admin"){
+        alert ("Login successfully");
+        window.location = "sucess.html"; // Redirecting to other page.
     }
     else {
         alert("Log in failed. Please Try Again: ");
