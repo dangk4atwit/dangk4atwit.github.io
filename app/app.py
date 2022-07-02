b72ead1eb9067b844428023fb747b3192a0b1d4c
dangk4atwit <60703094+dangk4atwit@users.noreply.github.com>
2022-07-02 17:44:06 -0400

    org and user

--- app/app.py
+++ app/app.py
@@ -16,6 +16,10 @@ nav = Navigation(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_BINDS'] = {
+    'organization': 'sqlite:///organization.db',
+    'timecard': 'sqlite:///timecard.db'
+}
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 login_manager = LoginManager()
@@ -33,7 +37,7 @@ nav.Bar('top', [
 def load_user(user_id):
     user = User.query.get(int(user_id))
     if user is None:
-        flash('You have been automatically logged out')
+        flash('You have been automatically logged ut')
     return user
 
 class User(db.Model, UserMixin):
@@ -43,13 +47,26 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(80), nullable=False, unique=True)
     username = db.Column(db.String(20), nullable=False, unique=True) # 20 characters
     password = db.Column(db.String(80), nullable=False)  # 80 characters
-
-    def __init__(self, fname, lname, email, username, password):
+    workId = db.Column(db.String(80), nullable=False)
+    pronouns = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80), nullable=False)
+    etype = db.Column(db.String(80), nullable=False)
+    pay = db.Column(db.String(80), nullable=False)
+    payInt = db.Column(db.String(80), nullable=False)
+    
+    def __init__(self, fname, lname, email, username, password, workId, pronouns, phone,
+    etype, pay, payInt):
         self.fname = fname
         self.lname = lname
         self.email = email
         self.username = username
         self.password = password
+        self.workId = workId
+        self.pronouns = pronouns
+        self.phone = phone
+        self.etype = etype
+        self.pay = pay
+        self.payInt = payInt
 
 db.create_all()
 db.session.commit()
@@ -58,6 +75,12 @@ class RegisterForm(FlaskForm):
     fname = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "First Name"})
     lname = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "Last Name"})
     email = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Email"})
+    workId = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Id"})
+    pronouns = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Pronouns"})
+    phone = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Phone"})
+    etype = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Employee Type"})
+    pay = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Pay Rate"})
+    payInt = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Pay Interval"})
     username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=25), EqualTo('confirm', message='Passwords must match')], render_kw={"placeholder": "Password"})
     confirm = PasswordField(render_kw={"placeholder": "Repeat Password"})
@@ -69,11 +92,87 @@ class RegisterForm(FlaskForm):
         if existing_user_username:
             raise ValidationError("That user is already registered.")
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(fname=form.fname.data, lname=form.lname.data, email=form.email.data, username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form=form)
+
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Password"})
     remember = BooleanField(false_values=(False, 'false', 0, '0'))
     submit = SubmitField("Login")
+
+class Org(db.Model, UserMixin):
+    __bind_key__ = 'organization'
+    id = db.Column(db.Integer, primary_key=True)
+    orgName = db.Column(db.String(80), nullable=False)
+    phoneorg = db.Column(db.String(80), nullable=False)
+    des = db.Column(db.String(80), nullable=False, unique=True)
+    orgAddress = db.Column(db.String(20), nullable=False, unique=True) 
+
+    
+
+    def __init__(self, orgName, phoneorg, des, orgAddress):
+        self.orgName = orgName
+        self.phoneorg = phoneorg
+        self.des = des
+        self.orgAddress = orgAddress
+
+db.create_all()
+db.session.commit()
+
+class OrgRegisterForm(FlaskForm):
+    orgName = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "Organization Name"})
+    phoneorg = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "Phone Number"})
+    des = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Description"})
+    orgAddress = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Organization Address"})
+
+    submit = SubmitField("Register")
+
+@app.route('/orgregister', methods=['GET', 'POST'])
+def org_register():
+    form = OrgRegisterForm()
+    
+    if form.validate_on_submit():
+        new_org = Org(orgName=form.orgName.data, phoneorg=form.phoneorg.data, des=form.des.data, orgAddress=form.orgAddress.data)
+        db.session.add(new_org)
+        db.session.commit()
+        return redirect(url_for('login'))
+    
+    return render_template('register_org.html', form=form)
+
+#class Time(db.Model, UserMixin):
+    __bind_key__ = 'timecard'
+
+    
+    
+
+    def __init__(self, fname, lname, email, username, password, workId, pronouns, phone,
+    etype, pay, payInt):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.username = username
+        self.password = password
+        self.workId = workId
+        self.pronouns = pronouns
+        self.phone = phone
+        self.etype = etype
+        self.pay = pay
+        self.payInt = payInt
+
+#db.create_all()
+#db.session.commit()
+
 
 class DashboardForm(FlaskForm):
     firstName = "Test"
@@ -203,18 +302,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(fname=form.fname.data, lname=form.lname.data, email=form.email.data, username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    
-    return render_template('register.html', form=form)
 
 @app.route('/')
 @login_required
