515fbe47589acc97f61186ffcd620ad0ba1c148f
hoj2atwit <43070855+hoj2atwit@users.noreply.github.com>
2022-07-04 21:24:27 -0400

    restructured python files and began researching modals for timecard

--- app/app.py
+++ app/app.py
@@ -1,26 +1,21 @@
 from audioop import add
 from enum import unique
+from cp_db import User, Org, app, db
 import bcrypt
-from flask import Flask, render_template, url_for, redirect, abort, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask import render_template, url_for, redirect, abort, flash
+# from flask_modals import Modal, render_template_modal
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from flask_navigation import Navigation
 from platformdirs import user_runtime_path
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import InputRequired, Length, ValidationError, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, FloatField
+from wtforms.validators import InputRequired, Length, ValidationError, EqualTo, NumberRange
 from flask_bcrypt import Bcrypt
+import phonenumbers
 
-app = Flask(__name__)
-nav = Navigation(app)
-db = SQLAlchemy(app)
+nav = Navigation(app)   
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_BINDS'] = {
-    'organization': 'sqlite:///organization.db',
-    'timecard': 'sqlite:///timecard.db'
-}
-app.config['SECRET_KEY'] = 'thisisasecretkey'
+# modal = Modal(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -40,50 +35,49 @@ def load_user(user_id):
         flash('You have been automatically logged ut')
     return user
 
-class User(db.Model, UserMixin):
-    
-    id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String(80), nullable=False)
-    lname = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False, unique=True)
-    username = db.Column(db.String(20), nullable=False, unique=True) # 20 characters
-    password = db.Column(db.String(80), nullable=False)  # 80 characters
-    workId = db.Column(db.String(80), nullable=False)
-    pronouns = db.Column(db.String(80), nullable=False)
-    phone = db.Column(db.String(80), nullable=False)
-    etype = db.Column(db.String(80), nullable=False)
-    pay = db.Column(db.String(80), nullable=False)
-    payInt = db.Column(db.String(80), nullable=False)
-    
-    def __init__(self, fname, lname, email, username, password, workId, pronouns, phone,
-    etype, pay, payInt):
-        self.fname = fname
-        self.lname = lname
-        self.email = email
-        self.username = username
-        self.password = password
-        self.workId = workId
-        self.pronouns = pronouns
-        self.phone = phone
-        self.etype = etype
-        self.pay = pay
-        self.payInt = payInt
+def isAdmin():
+    if str(current_user.workId)[0:1] == "69":
+        return True
+    return False
 
-db.create_all()
-db.session.commit()
+def adaptRegular():
+    nav.Bar('top', [
+    nav.Item('Dashboard', 'dashboard'),
+    nav.Item('Profile', 'profile'),
+    nav.Item('Timecard', 'timecard'),
+    nav.Item('Verify', 'verify'),])
+
+def adaptAdmin():
+    if isAdmin():
+        nav.Bar('top', [
+            nav.Item('Dashboard', 'dashboard'),
+            nav.Item('Profile', 'profile'),
+            nav.Item('Timecard', 'timecard'),
+            nav.Item('Verify', 'verify'),
+            nav.Item('Management', 'management'),])
+    else:
+        adaptRegular()
+
+
+
+
+
+####################################################            FORMS & PAGES              #################################################################################
+
 
 class RegisterForm(FlaskForm):
     fname = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "First Name"})
     lname = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "Last Name"})
     email = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Email"})
-    workId = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Id"})
+    workId = IntegerField(validators=[InputRequired(), NumberRange(min=10000000, max=99999999)], render_kw={"placeholder": "Id"})
     pronouns = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Pronouns"})
-    phone = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Phone"})
+    phone = StringField(validators=[InputRequired(), Length(min=10, max=15)], render_kw={"placeholder": "Phone"})
     etype = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Employee Type"})
-    pay = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Pay Rate"})
+    pay = FloatField(validators=[InputRequired(), NumberRange(min=7.25)], render_kw={"placeholder": "Pay Rate"})
     payInt = StringField(validators=[InputRequired(), Length(min=5, max=25)], render_kw={"placeholder": "Pay Interval"})
     username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=25), EqualTo('confirm', message='Passwords must match')], render_kw={"placeholder": "Password"})
+    profileImgUrl = StringField(validators=[Length(max=120)], render_kw={"placeholder": "Profile Image URL"})
     confirm = PasswordField(render_kw={"placeholder": "Repeat Password"})
 
     submit = SubmitField("Register")
@@ -91,7 +85,26 @@ class RegisterForm(FlaskForm):
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
+            print("username")
             raise ValidationError("That user is already registered.")
+    
+    def validate_workId(self, workId):
+        existing_workId_username = User.query.filter_by(workId=workId.data).first()
+        if existing_workId_username:
+            print("workID")
+            raise ValidationError("That id is already registered.")
+        
+    def validate_phone(self, phone):
+        try:
+            p = phonenumbers.parse(phone.data, None)
+            if not phonenumbers.is_valid_number(p):
+                print("Phone Number valid number")
+                raise ValueError()
+        except (phonenumbers.phonenumberutil.NumberParseException, ValueError) as e:
+            print(phone.data)
+            print("Phone Number format")
+            print(e)
+            raise ValidationError('Invalid phone number')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -99,37 +112,17 @@ def register():
     
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(fname=form.fname.data, lname=form.lname.data, email=form.email.data, username=form.username.data, password=hashed_password)
+        new_user = User(fname=form.fname.data, lname=form.lname.data, email=form.email.data,
+                        username=form.username.data, password=hashed_password, workId=form.workId.data,
+                        pronouns=form.pronouns.data, phone=form.phone.data, etype=form.etype.data,
+                        pay=int(round(form.pay.data, 2)*100), payInt=form.payInt.data, pImgURL=form.profileImgUrl.data)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
     
     return render_template('register.html', form=form)
 
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Password"})
-    remember = BooleanField(false_values=(False, 'false', 0, '0'))
-    submit = SubmitField("Login")
 
-class Org(db.Model, UserMixin):
-    __bind_key__ = 'organization'
-    id = db.Column(db.Integer, primary_key=True)
-    orgName = db.Column(db.String(80), nullable=False)
-    phoneorg = db.Column(db.String(80), nullable=False)
-    des = db.Column(db.String(80), nullable=False, unique=True)
-    orgAddress = db.Column(db.String(20), nullable=False, unique=True) 
-
-    
-
-    def __init__(self, orgName, phoneorg, des, orgAddress):
-        self.orgName = orgName
-        self.phoneorg = phoneorg
-        self.des = des
-        self.orgAddress = orgAddress
-
-db.create_all()
-db.session.commit()
 
 class OrgRegisterForm(FlaskForm):
     orgName = StringField(validators=[InputRequired(), Length(min=2, max=25)], render_kw={"placeholder": "Organization Name"})
@@ -151,134 +144,13 @@ def org_register():
     
     return render_template('register_org.html', form=form)
 
-#class Time(db.Model, UserMixin):
-    __bind_key__ = 'timecard'
-
-    
-    
-
-    def __init__(self, fname, lname, email, username, password, workId, pronouns, phone,
-    etype, pay, payInt):
-        self.fname = fname
-        self.lname = lname
-        self.email = email
-        self.username = username
-        self.password = password
-        self.workId = workId
-        self.pronouns = pronouns
-        self.phone = phone
-        self.etype = etype
-        self.pay = pay
-        self.payInt = payInt
-
-#db.create_all()
-#db.session.commit()
 
 
-class DashboardForm(FlaskForm):
-    firstName = "Test"
-    lastName = "Name"
-    title = "Test Title"
-    isAdmin = False
-    
-class TimecardForm(FlaskForm):
-    firstName = "Test"
-    lastName = "Name"
-    title = "Test Title"
-    isAdmin = False
-    
-class VerifyForm(FlaskForm):
-    firstName = "Test"
-    lastName = "Name"
-    title = "Test Title"
-    isAdmin = False
-    
-class ProfileForm(FlaskForm):
-    firstName = "Admin"
-    lastName = "Admin"
-    pronouns="(He/Him)"
-    title = "Administrator"
-    employmentEmail = "admin@checkpointmail.com"
-    employeeID = "0000000001";
-    employmentType = "Tenure"
-    payRate = 420
-    payInterval = "century"
-    superior = "Mr. Boss"
-    orgName = "Wentworth Institute of Technology"
-    orgAddress="550 Huntington Ave, Boston, MA 02115"
-    orgLeader="Mark A. Thompson"
-    isAdmin = True
-    
-class ManagementForm(FlaskForm):
-    firstName = "Admin"
-    lastName = "Admin"
-    title = "Administrator"
-    isAdmin = False
-
-def adaptAdmin():
-    nav.Bar('top', [
-            nav.Item('Dashboard', 'dashboard'),
-            nav.Item('Profile', 'profile'),
-            nav.Item('Timecard', 'timecard'),
-            nav.Item('Verify', 'verify'),
-            nav.Item('Management', 'management'),])
-    
-def adaptRegular():
-    nav.Bar('top', [
-    nav.Item('Dashboard', 'dashboard'),
-    nav.Item('Profile', 'profile'),
-    nav.Item('Timecard', 'timecard'),
-    nav.Item('Verify', 'verify'),])
-
-@app.route('/profile', methods=['GET', 'POST'])
-@login_required
-def profile():
-    form = ProfileForm()
-    if form.isAdmin:
-        adaptAdmin()
-    else:
-        adaptRegular()
-    return render_template('profile.html', form=form)
-
-@app.route('/management', methods=['GET', 'POST'])
-@login_required
-def management():
-    form = ManagementForm()
-    if form.isAdmin:
-        adaptAdmin()
-    else:
-        adaptRegular()
-    return render_template('management.html', form=form)
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    form = DashboardForm()
-    if form.isAdmin:
-        adaptAdmin()
-    else:
-        adaptRegular()
-    return render_template('dashboard.html', form=form)
-
-@app.route('/timecard', methods=['GET', 'POST'])
-@login_required
-def timecard():
-    form = TimecardForm()
-    if form.isAdmin:
-        adaptAdmin()
-    else:
-        adaptRegular()
-    return render_template('timecard.html', form=form)
-
-@app.route('/verify', methods=['GET', 'POST'])
-@login_required
-def verify():
-    form = VerifyForm()
-    if form.isAdmin:
-        adaptAdmin()
-    else:
-        adaptRegular()
-    return render_template('verify.html', form=form)
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=25)], render_kw={"placeholder": "Password"})
+    remember = BooleanField(false_values=(False, 'false', 0, '0'))
+    submit = SubmitField("Login")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -297,6 +169,98 @@ def login():
             
     return render_template('login.html', form=form)
 
+
+
+class DashboardForm(FlaskForm):
+    firstName = "Test"
+    lastName = "Name"
+    title = "Test Title"
+    isAdmin = False
+    
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    form = DashboardForm()
+    adaptAdmin()
+    return render_template('dashboard.html', form=form)
+    
+    
+    
+    
+class TimecardForm(FlaskForm):
+    firstName = "Test"
+    lastName = "Name"
+    title = "Test Title"
+    isAdmin = False
+    
+@app.route('/timecard', methods=['GET', 'POST'])
+@login_required
+def timecard():
+    form = TimecardForm()
+    adaptAdmin()
+    return render_template('timecard.html', form=form)
+
+
+    
+class VerifyForm(FlaskForm):
+    firstName = "Test"
+    lastName = "Name"
+    title = "Test Title"
+    isAdmin = False
+    
+@app.route('/verify', methods=['GET', 'POST'])
+@login_required
+def verify():
+    form = VerifyForm()
+    adaptAdmin()
+    return render_template('verify.html', form=form)
+
+
+    
+class ProfileForm(FlaskForm):
+    firstName = "Admin"
+    lastName = "Admin"
+    pronouns="(He/Him)"
+    title = "Administrator"
+    employmentEmail = "admin@checkpointmail.com"
+    employeeID = "0000000001";
+    employmentType = "Tenure"
+    payRate = 420
+    payInterval = "century"
+    superior = "Mr. Boss"
+    orgName = "Wentworth Institute of Technology"
+    orgAddress="550 Huntington Ave, Boston, MA 02115"
+    orgLeader="Mark A. Thompson"
+    isAdmin = True
+    
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = ProfileForm()
+    adaptAdmin()
+    return render_template('profile.html', form=form)    
+
+    
+    
+class ManagementForm(FlaskForm):
+    firstName = "Admin"
+    lastName = "Admin"
+    title = "Administrator"
+    isAdmin = False
+
+@app.route('/management', methods=['GET', 'POST'])
+@login_required
+def management():
+    form = ManagementForm()
+    adaptAdmin()
+    return render_template('management.html', form=form)
+
+
+
+
+
+
+
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
@@ -308,6 +272,15 @@ def logout():
 @login_required
 def home():
     return redirect(url_for('dashboard'))
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/cp_db.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_BINDS'] = {
+    'organization': 'sqlite:///organizations.db',
+    'timecard': 'sqlite:///timecards.db'
+}
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+class User(db.Model, UserMixin):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(80), nullable=False)
+    lname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(20), nullable=False, unique=True) # 20 characters
+    password = db.Column(db.String(80), nullable=False)  # 80 characters
+    workId = db.Column(db.Integer, nullable=False)
+    pronouns = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80), nullable=False)
+    etype = db.Column(db.String(80), nullable=False)
+    pay = db.Column(db.Integer, nullable=False)
+    payInt = db.Column(db.String(80), nullable=False)
+    pImgURL = db.Column(db.String(120), nullable=False)
+    
+    def __init__(self, fname, lname, email, username, password, workId, pronouns, phone,
+    etype, pay, payInt, pImgURL="None"):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.username = username    
+        self.password = password
+        self.workId = workId
+        self.pronouns = pronouns
+        self.phone = phone
+        self.etype = etype
+        self.pay = pay
+        self.payInt = payInt
+        self.pImgURL = pImgURL
+        
+db.create_all()
+db.session.commit()
+        
+class Org(db.Model, UserMixin):
+    __bind_key__ = 'organization'
+    id = db.Column(db.Integer, primary_key=True)
+    orgName = db.Column(db.String(80), nullable=False)
+    phoneorg = db.Column(db.String(80), nullable=False)
+    des = db.Column(db.String(80), nullable=False, unique=True)
+    orgAddress = db.Column(db.String(20), nullable=False, unique=True) 
+    logoURL = db.Column(db.String(120), nullable=False)
+    bannerURL = db.Column(db.String(120), nullable=False)
+    
+
+    def __init__(self, orgName, phoneorg, des, orgAddress):
+        self.orgName = orgName
+        self.phoneorg = phoneorg
+        self.des = des
+        self.orgAddress = orgAddress
+        
+db.create_all()
+db.session.commit()
+        
+#class Time(db.Model, UserMixin):
+    # __bind_key__ = 'timecard'
+
+    
+    
+
+    # def __init__(self, fname, lname, email, username, password, workId, pronouns, phone,
+    # etype, pay, payInt):
+    #     self.fname = fname
+    #     self.lname = lname
+    #     self.email = email
+    #     self.username = username
+    #     self.password = password
+    #     self.workId = workId
+    #     self.pronouns = pronouns
+    #     self.phone = phone
+    #     self.etype = etype
+    #     self.pay = pay
+    #     self.payInt = payInt
+#db.create_all()
+#db.session.commit()
+
+if __name__ == '__main__':
+    db.drop_all()
+    app.run(debug=True)
