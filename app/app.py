22f323b7f3b949788f1debc6ec9d3d85ea82e095
dangk4atwit <60703094+dangk4atwit@users.noreply.github.com>
2022-07-20 12:35:50 -0400

    face detector with opencv

--- app/app.py
+++ app/app.py
@@ -29,9 +29,22 @@ def gen_frames():
         if not success:
             break
         else:
+            detector=cv2.CascadeClassifier('app/Haarcascades/haarcascade_frontalface_default.xml')
+            eye_cascade = cv2.CascadeClassifier('app/Haarcascades/haarcascade_eye.xml')
+            faces=detector.detectMultiScale(frame,1.1,7)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                #drawing the outline box to look for face features
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
             ret, buffer=cv2.imencode('.jpg',frame)
             frame=buffer.tobytes()
-        yield(b'--frame\r\n'
+            yield(b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
